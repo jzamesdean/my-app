597d16807ac61c42939bbd6b0545af4f41ac5146
jzamesdean <k50524202@naver.com>
2023-09-18 11:05:26 +0900

    learn react 0

---
+++ src/learnreact0/App.js
@@ -0,0 +1,59 @@
+function Profile() {
+    return (
+        <img
+            src="https://i.imgur.com/MK3eW3Am.jpg"
+            alt="Katherine Johnson"
+        />
+    );
+}
+
+function Gallery() {
+    return (
+        <section>
+            <h1>Amazing scientists</h1>
+            <Profile/>
+            <Profile/>
+            <Profile/>
+        </section>
+    );
+}
+
+function Profile1() {
+    return (
+      <img
+        src="https://i.imgur.com/lICfvbD.jpg"
+        alt="Aklilu Lemma"
+      />
+    );
+  }
+
+function Profile2() {
+    return <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+}
+
+function Profile3() {
+    return (
+        <img
+            src="https://i.imgur.com/QIrZWGIs.jpg"
+            alt="Alan L. Hart"
+        />
+    );
+}
+
+function Gallery1() {
+    return (
+        <section>
+        <h1>Amazing Scientists</h1>
+        <Profile3 />
+        <Profile3 />
+        <Profile3 />
+        </section>
+    )
+}
+
+function Congratulations() {
+    return <h1>Good Job!</h1>
+}
+
+
+export default Congratulations
